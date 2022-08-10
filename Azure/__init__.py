760cf8ec0d0afd380994bd41d7a5634537eb95a9
lochov <antoniocarbonesarmiento98@gmail.com>
2022-08-10 12:48:44 +0200

    added __init__ files

---
+++ Azure/__init__.py
@@ -0,0 +1,13 @@
+""" ADD All AWS Scripts here to be imported by cloudscripts.py"""
+__all__ = [
+"aws_s3_inventory_csv",
+"aws_ec2_inventory_csv",
+"aws_ec2_publicip_inventory_csv",
+"aws_elb_inventory_csv",
+"aws_lambda_inventory_csv",
+"AWS_SecurityHub_CIS_csv",
+"AWS_SecurityHubCollector_csv",
+"aws_subnets_inventory_csv",
+]
+
+
