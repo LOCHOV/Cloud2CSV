c4b9c66adcd66047517d30aec0c22a6c6ee00732
lochov <antoniocarbonesarmiento98@gmail.com>
2022-08-11 14:47:49 +0200

    Completed Parser

--- cloudscripts.py
+++ cloudscripts.py
@@ -34,34 +34,25 @@ class user_selection:
             import AWS.aws_subnets_inventory_csv
             AWS.aws_subnets_inventory_csv.main()
         else:
-            print("Module was not found. Maybe it is misspelled or I just didnt have time to develop it yet :)")
             print(self.message)
 
     def azure_selected(self):
         print("You selected: " + self.script)
-        if self.script == "Azure_Microsoft_Defender_CIS_Findings":
-            import AZR.Azure_CIS_single_sub
-            AZR.Azure_CIS_single_sub.main()
-        elif self.script == "Azure_VM_instances":
+        if self.script == "Azure_VM_instances":
             import AZR.azure_vm_inventory_csv
             AZR.azure_vm_inventory_csv.main()
         elif self.script == "Azure_Read_Exposed_Blob_Anonymously":
             import AZR.azureblobread
             AZR.azureblobread.main()
         else:
-            print("Module was not found. Maybe it is misspelled or I just didn't have time to develop it yet :)")
             print(self.message)
 
     def gcp_selected(self):
         print("You selected: " + self.script)
-        if self.script == "GCP_Security_Command_Center_CIS_Findings":
-            import GCP.GCP_CIS_scc_singleproject_csv
-            GCP.GCP_CIS_scc_singleproject_csv.main()
-        elif self.script == "GCP_VM_Instances":
+        if self.script == "GCP_VM_Instances":
             import GCP.gcp_vm_inventory_csv
             GCP.gcp_vm_inventory_csv.main()
         else:
-            print()
             print(self.message)
 
 
@@ -69,31 +60,36 @@ def main():
     """ Define the options and the help message for the argparser"""
     options_aws = ["AWS_EC2_Instances", "AWS_EC2_Public_IPs", "AWS_EC2_ELB", "AWS_Lambda_Functions", "AWS_S3_Buckets",
                    "AWS_SecurityHub_CIS_Findings", "AWS_SecurityHub_All_Findings", "AWS_VPC_Subnets"]
-    options_azure = ["Azure_Read_Exposed_Blob_Anonymously", "Azure_VM_instances",
-                     "Azure_Microsoft_Defender_CIS_Findings"]
-    options_gcp = ["GCP_Security_Command_Center_CIS_Findings", "GCP_VM_Instances"]
+    options_azure = ["Azure_Read_Exposed_Blob_Anonymously", "Azure_VM_instances"]
+    options_gcp = ["GCP_VM_Instances"]
 
     """ The message is splitted in two parts as a list in order to be able to chose one"""
-    message = ['Allowed values are:\n\n------ AWS ------\n' + '\n'.join(options_aws) \
+    message = ["Module was not found. Maybe it is misspelled or I just didn't have time to develop it yet :)\n",
+               "Allowed values for -s, --script\n\n",
+               '------ AWS ------\n' + '\n'.join(options_aws) \
                + "\n\n------ Azure ------\n" + '\n'.join(options_azure) \
-               + "\n\n------ GCP ------\n" + '\n'.join(options_gcp),
-               "\nModule was not found. Maybe it is misspelled or I just didn't have time to develop it yet :)"]
+               + "\n\n------ GCP ------\n" + '\n'.join(options_gcp)
+               ]
+    help_message = message[2]
+    wrong_selection_message = message[0] + message[1] + message[2]
 
     """ Argument Parser """
     parser = argparse.ArgumentParser(description='List the content of a folder', formatter_class=RawTextHelpFormatter)
-    parser.add_argument('-s', '--script', dest="arg_value", metavar='', type=str, help=message[0])
+    parser.add_argument('-s', '--script', dest="arg_value", metavar='', type=str, help=help_message)
     args = parser.parse_args()
-    user = user_selection(args.arg_value, message)  # Add the value to the user class
+    user = user_selection(args.arg_value, wrong_selection_message)  # Add the value to the user class
 
     """ Run Script based on the provided value """
-    if "AWS_" in args.arg_value:
+    if not args.arg_value:
+        print(wrong_selection_message)
+    elif "AWS_" in args.arg_value:
         user.aws_selected()
     elif "Azure_" in args.arg_value:
         user.azure_selected()
     elif "GCP_" in args.arg_value:
         user.gcp_selected()
     else:
-        print(message[1] + message[0])
+        print(wrong_selection_message)
 
 
 main()
